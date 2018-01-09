44814e9755600bc7c91a536e06b627adeab262ab
Alessio Ceroni <alessio.ceroni@gmail.com>
2018-01-09 21:19:21 +0100

    deploy front-end with spring

--- server/src/main/java/com/github/aceroni75/gwtboot/server/JerseyConfig.java
+++ server/src/main/java/com/github/aceroni75/gwtboot/server/JerseyConfig.java
@@ -4,7 +4,10 @@ import com.github.aceroni75.gwtboot.shared.resource.ResourcePackage;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.springframework.stereotype.Component;
 
+import javax.ws.rs.ApplicationPath;
+
 @Component
+@ApplicationPath("/api")
 public class JerseyConfig extends ResourceConfig {
     public JerseyConfig() {
         packages(ResourcePackage.class.getPackage().getName());
